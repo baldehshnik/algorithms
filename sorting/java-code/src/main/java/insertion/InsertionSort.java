3d296ecd3087d3750f873e1177694d0a8479ddcb
Vladislav Shcherba <v.shcherba.dev@gmail.com>
2023-10-05 13:42:21 +0300

    insertion sort was documented (Java)

--- sorting/java-code/src/main/java/insertion/InsertionSort.java
+++ sorting/java-code/src/main/java/insertion/InsertionSort.java
@@ -7,8 +7,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The class is used to implement insertion sort algorithm for collections and not primitive arrays.
+ *
+ * @author Shcherba Vladislav
+ * @version 1.0
+ * */
 public class InsertionSort<T extends Comparable<T>> {
 
+    /**
+     * Implements insertion sort and sorts list of <T> type.
+     * This method sorts the given list and returns nothing.
+     *
+     * @param list - list that should be sorted
+     * */
     public void sort(@NotNull List<T> list) {
         for (int i = 1; i < list.size(); i++) {
             if (list.get(i - 1).compareTo(list.get(i)) > 0) {
@@ -28,6 +40,12 @@ public class InsertionSort<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Implements insertion sort and sorts array of <T> type.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(@NotNull T[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1].compareTo(array[i]) > 0) {
@@ -49,6 +67,12 @@ public class InsertionSort<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Uses insertion sort {@link InsertionSort#sort(List)} to sort a list copy of <T> type and returns him.
+     *
+     * @param list - list a copy of witch should be sorted
+     * @return - new list in the sorted order
+     * */
     public List<T> sorted(@NotNull List<T> list) {
         List<T> newList = new ArrayList<>(list);
         sort(newList);
@@ -56,6 +80,12 @@ public class InsertionSort<T extends Comparable<T>> {
         return newList;
     }
 
+    /**
+     * Uses insertion sort {@link InsertionSort#sort(Comparable[])} to sort an array copy of <T> type and returns him.
+     *
+     * @param array - array a copy of witch should be sorted
+     * @return - new array in the sorted order
+     * */
     public T[] sorted(@NotNull T[] array) {
         T[] newArray = Arrays.copyOf(array, array.length);
         sort(newArray);
--- sorting/java-code/src/main/java/insertion/PrimitivesInsertionSort.java
+++ sorting/java-code/src/main/java/insertion/PrimitivesInsertionSort.java
@@ -2,8 +2,20 @@ package insertion;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The class is used to implement insertion sort algorithm for int[] and double[] arrays.
+ *
+ * @author Shcherba Vladislav
+ * @version 1.0
+ * */
 public class PrimitivesInsertionSort {
 
+    /**
+     * Implements insertion sort and sorts int[] array.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(int @NotNull [] array) {
         for (int i = 1; i < array.length; i++) {
             int index = i;
@@ -21,6 +33,12 @@ public class PrimitivesInsertionSort {
         }
     }
 
+    /**
+     * Implements insertion sort and sorts double[] array.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(double @NotNull [] array) {
         for (int i = 1; i < array.length; i++) {
             int index = i;
@@ -38,6 +56,12 @@ public class PrimitivesInsertionSort {
         }
     }
 
+    /**
+     * Uses insertion sort {@link PrimitivesInsertionSort#sort(int[])} to sort an int[] array.
+     * This method sorts the given array and returns new int[] array.
+     *
+     * @param array - array a copy of witch should be sorted
+     * */
     public int[] sorted(int @NotNull [] array) {
         int[] newArray = new int[array.length];
         System.arraycopy(array, 0, newArray, 0, array.length);
@@ -46,6 +70,12 @@ public class PrimitivesInsertionSort {
         return newArray;
     }
 
+    /**
+     * Uses insertion sort {@link PrimitivesInsertionSort#sort(double[])} to sort a double[] array.
+     * This method sorts the given array and returns new double[] array.
+     *
+     * @param array - array a copy of witch should be sorted
+     * */
     public double[] sorted(double @NotNull [] array) {
         double[] newArray = new double[array.length];
         System.arraycopy(array, 0, newArray, 0, array.length);
