78125e9844a1c80049cc9057c175680cf8ecc723
Vladislav Shcherba <v.shcherba.dev@gmail.com>
2023-10-29 13:18:30 +0300

    merge sort was documented (Java)

--- sorting/java-code/src/main/java/merge/MergeSort.java
+++ sorting/java-code/src/main/java/merge/MergeSort.java
@@ -7,8 +7,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * The class is used to implement the merge sort algorithm for collections and not primitive arrays.
+ *
+ * @author Shcherba Vladislav
+ * @version 1.0
+ * */
 public class MergeSort<T extends Comparable<T>> {
 
+    /**
+     * Implements merge sort and sorts array of <T> type.
+     * This method uses the merge function {@link MergeSort#merge(Comparable[], Comparable[], Comparable[])} to merge two separated arrays.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(T @NotNull [] array) {
         if (array.length <= 1) return;
 
@@ -21,6 +34,15 @@ public class MergeSort<T extends Comparable<T>> {
         merge(array, firstPartOfArray, secondPartOfArray);
     }
 
+    /**
+     * Implements merge sort and sorts list of <T> type.
+     * This method uses the merge function {@link MergeSort#merge(List, Comparable[], Comparable[])} to merge two separated arrays.
+     * This method sorts the given list and returns nothing.
+     *
+     * @param list - list that should be sorted
+     *
+     * @throws ClassCastException when can't create array of list type
+     * */
     @SuppressWarnings("unchecked")
     public void sort(@NotNull List<T> list) throws ClassCastException {
         if (list.size() <= 1) return;
@@ -44,6 +66,12 @@ public class MergeSort<T extends Comparable<T>> {
         merge(list, firstPartOfList, secondPartOfList);
     }
 
+    /**
+     * Uses merge sort {@link MergeSort#merge(Comparable[], Comparable[], Comparable[])} to sort an array copy of <T> type and returns him.
+     *
+     * @param array - array a copy of which should be sorted
+     * @return - new array in the sorted order
+     * */
     public T[] sorted(T  @NotNull [] array) {
         T[] newArray = Arrays.copyOf(array, array.length);
         sort(newArray);
@@ -51,6 +79,14 @@ public class MergeSort<T extends Comparable<T>> {
         return newArray;
     }
 
+    /**
+     * Uses merge sort {@link MergeSort#merge(List, Comparable[], Comparable[])} to sort a list copy of <T> type and returns him.
+     *
+     * @param list - list a copy of which should be sorted
+     * @return - new list in the sorted order
+     *
+     * @throws ClassCastException because uses {@link MergeSort#sort(List)} function
+     * */
     public List<T> sorted(@NotNull List<T> list) {
         List<T> newList = new ArrayList<>(list);
         sort(newList);
@@ -58,6 +94,13 @@ public class MergeSort<T extends Comparable<T>> {
         return newList;
     }
 
+    /**
+     * Merges the given two arrays in the given destination array in the sorted order.
+     *
+     * @param destination - array that should contain the given arrays in the sorted order and in the right place
+     * @param array1 - array that should be merged with array2 variable
+     * @param array2 - array that should be merged with array1 variable
+     * */
     private void merge(T @NotNull [] destination, T @NotNull [] array1, T @NotNull [] array2) {
         int index1 = 0, index2 = 0;
         int maxLength = array1.length + array2.length;
@@ -79,6 +122,13 @@ public class MergeSort<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Merges the given two arrays in the given destination list in the sorted order.
+     *
+     * @param destination - list that should contain the given arrays in the sorted order and in the right place
+     * @param array1 - array that should be merged with array2 variable
+     * @param array2 - array that should be merged with array1 variable
+     * */
     private void merge(@NotNull List<T> destination, T @NotNull [] array1, T @NotNull [] array2) {
         int index1 = 0, index2 = 0;
         int maxLength = array1.length + array2.length;
--- sorting/java-code/src/main/java/merge/PrimitivesMergeSort.java
+++ sorting/java-code/src/main/java/merge/PrimitivesMergeSort.java
@@ -2,8 +2,21 @@ package merge;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The class is used to implement merge sort algorithm for int[] and double[] arrays.
+ *
+ * @author Shcherba Vladislav
+ * @version 1.0
+ * */
 public class PrimitivesMergeSort {
 
+    /**
+     * Implements merge sort and sorts int[] array.
+     * This method uses the merge function {@link PrimitivesMergeSort#merge(int[], int[], int[])} to merge two separated arrays.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(int @NotNull [] array) {
         if (array.length <= 1) return;
 
@@ -20,6 +33,13 @@ public class PrimitivesMergeSort {
         merge(array, firstPartOfArray, secondPartOfArray);
     }
 
+    /**
+     * Implements merge sort and sorts double[] array.
+     * This method uses the merge function {@link PrimitivesMergeSort#merge(double[], double[], double[])} to merge two separated arrays.
+     * This method sorts the given array and returns nothing.
+     *
+     * @param array - array that should be sorted
+     * */
     public void sort(double @NotNull [] array) {
         if (array.length <= 1) return;
 
@@ -36,6 +56,12 @@ public class PrimitivesMergeSort {
         merge(array, firstPartOfArray, secondPartOfArray);
     }
 
+    /**
+     * Uses merge sort {@link PrimitivesMergeSort#sort(int[])} to sort an int[] array.
+     * This method sorts the given array and returns new int[] array.
+     *
+     * @param array - array a copy of which should be sorted
+     * */
     public int[] sorted(int @NotNull [] array) {
         int[] newArray = new int[array.length];
         System.arraycopy(array, 0, newArray, 0, newArray.length);
@@ -44,6 +70,12 @@ public class PrimitivesMergeSort {
         return newArray;
     }
 
+    /**
+     * Uses merge sort {@link PrimitivesMergeSort#sort(double[])} to sort a double[] array.
+     * This method sorts the given array and returns new double[] array.
+     *
+     * @param array - array a copy of which should be sorted
+     * */
     public double[] sorted(double @NotNull [] array) {
         double[] newArray = new double[array.length];
         System.arraycopy(array, 0, newArray, 0, newArray.length);
@@ -52,6 +84,13 @@ public class PrimitivesMergeSort {
         return newArray;
     }
 
+    /**
+     * Merges the given two int[] arrays in the given int[] destination array in the sorted order.
+     *
+     * @param destination - the int[] array that should contain the given arrays in the sorted order and in the right place
+     * @param array1 - the int[] array that should be merged with array2 variable
+     * @param array2 - the int[] array that should be merged with array1 variable
+     * */
     private void merge(int @NotNull [] destination, int @NotNull [] array1, int @NotNull [] array2) {
         int index1 = 0, index2 = 0;
         int maxLength = array1.length + array2.length;
@@ -73,6 +112,13 @@ public class PrimitivesMergeSort {
         }
     }
 
+    /**
+     * Merges the given two double[] arrays in the given double[] destination array in the sorted order.
+     *
+     * @param destination - the double[] array that should contain the given arrays in the sorted order and in the right place
+     * @param array1 - the double[] array that should be merged with array2 variable
+     * @param array2 - the double[] array that should be merged with array1 variable
+     * */
     private void merge(double @NotNull [] destination, double @NotNull [] array1, double @NotNull [] array2) {
         int index1 = 0, index2 = 0;
         int maxLength = array1.length + array2.length;
